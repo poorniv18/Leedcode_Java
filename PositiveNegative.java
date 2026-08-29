faaa5c11547aad4e5a0e7206ac8e736b82451afe
POORNIMA.V <poorniv192007@gmail.com>
2026-08-29 21:30:53 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,13 @@
+class PositiveNegative{
+    public static void main(String[] args){
+        int n=-5;
+        if(n>0)
+         System.out.println("Positive Number");
+         else if(n<0)
+
+        System.out.println("Negative Number");
+        else
+        System.out.println("Zero");
+    
+}
+}
